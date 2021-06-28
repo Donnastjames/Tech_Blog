15fd62085d45a06f8eecfe7f235f07b6e2c4f76d
Donna <donnastjames@comcast.net>
2021-06-27 23:20:16 -0700

    Added models/index.js

---
+++ Develop/models/index.js
@@ -0,0 +1,21 @@
+const User = require('./User');
+const Blog_Comment = require('./Blog_Comment');
+const Blog_Post = require('./Blog_Post');
+
+User.hasMany(Blog_Post, {
+  foreignKey: 'user_id',
+});
+
+Blog_Post.belongsTo(User, {
+  foreignKey: 'user_id'
+});
+
+User.hasMany(Blog_Comment, {
+  foreignKey: 'user_id',
+});
+
+Blog_Comment.belongsTo(User, {
+  foreignKey: 'user_id'
+});
+
+module.exports = { User, Blog_Comment, Blog_Post };
