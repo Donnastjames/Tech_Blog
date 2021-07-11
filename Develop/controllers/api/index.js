c1678eeca0c934d8b069606d4a3f899698e5ec16
Donna <donnastjames@comcast.net>
2021-07-11 01:11:50 -0700

    Login and Sign Up form submits work

--- Develop/controllers/api/index.js
+++ Develop/controllers/api/index.js
@@ -1,7 +1,9 @@
 const router = require('express').Router();
 
 const blogPostRoutes = require('./blogPostRoutes');
+const userRoutes = require('./userRoutes');
 
 router.use('/blogPosts', blogPostRoutes);
+router.use('/users', userRoutes);
 
 module.exports = router;
